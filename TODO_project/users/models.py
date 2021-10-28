b2660ee9e9956fbb0fc6701f8b797404edcb488c
codezerodawn <codezerodawn@gmail.com>
2021-10-28 13:08:22 +0300

    Add user model in users app

--- TODO_project/users/models.py
+++ TODO_project/users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class TodoUser(AbstractUser):
+    username = models.CharField(
+        verbose_name='имя пользователя',
+        max_length=128,
+        blank=False,
+        null=False)
+    firstname = models.CharField(
+        verbose_name='имя',
+        max_length=128,
+        blank=False,
+        null=False)
+    lastname = models.CharField(
+        verbose_name='фамилия',
+        max_length=128,
+        blank=False,
+        null=False)
+    email = models.EmailField(
+        verbose_name='адрес эл. почты',
+        max_length=254,
+        blank=False,
+        null=False,
+        unique=True)
+
