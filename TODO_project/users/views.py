e84354fd71450fb819e508a691728b7e2f052d45
codezerodawn <codezerodawn@gmail.com>
2021-10-28 15:46:12 +0300

    Create view set for TodoUser model in users app

--- TODO_project/users/views.py
+++ TODO_project/users/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import TodoUser
+from .serializers import TodoUserModelSerializer
 
-# Create your views here.
+
+class TodoUserModelViewSet(ModelViewSet):
+    queryset = TodoUser.objects.all()
+    serializer_class = TodoUserModelSerializer
