0dacc7d5d5822a8d70532b2160bd56c02338a903
codezerodawn <codezerodawn@gmail.com>
2021-10-28 03:40:17 +0300

    update project settings for managing with dynaconf and path to base dir

--- TODO_project/TODO_project/settings.py
+++ TODO_project/TODO_project/settings.py
@@ -10,20 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
+import os, json
+from dynaconf import settings as config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=c-w2ysabgl-ee59ko_tiw79bczk%qh0+uy8%zh7f7ha43_2i5'
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config.DEBUG
 
 ALLOWED_HOSTS = []
 
