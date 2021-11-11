fc29cbc40c810f5ba6a16e46818cb03b35695b37
codezerodawn <codezerodawn@gmail.com>
2021-11-11 14:12:33 +0300

    Create views in TODO app

--- TODO_project/TODO/views.py
+++ TODO_project/TODO/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Project, ToDo
+from .serializers import ProjectModelSerializer, ToDoModelSerializer
+
+
+class ProjectModelViewSet(ModelViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    queryset = Project.objects.all()
+    serializer_class = ProjectModelSerializer
+
+
+class ToDoModelViewSet(ModelViewSet):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoModelSerializer
+
