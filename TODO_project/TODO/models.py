0386afa491267418c0398a9ef74d0eafc557fb9c
codezerodawn <codezerodawn@gmail.com>
2021-11-11 12:54:59 +0300

    Add models Project and ToDo

--- TODO_project/TODO/models.py
+++ TODO_project/TODO/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
+from users.models import TodoUser
+
+
+class Project(models.Model):
+    name = models.CharField(
+        verbose_name='название проекта',
+        max_length=128)
+    repo_link = models.CharField(
+        verbose_name='ссылка на репозиторий',
+        max_length=256)
+    authors = models.ManyToManyField(
+        TodoUser,
+        verbose_name='авторы проекта')
+
+    def __str__(self):
+        return f'Проект: {self.name}'
+
+class ToDo(models.Model):
+    project = models.ForeignKey(
+        Project,
+        models.PROTECT,
+        verbose_name='проект заметки')
+    title = models.CharField(
+        verbose_name='заголовок заметки',
+        blank=False)
+    text = models.TextField(
+        verbose_name='текст заметки',
+        blank=True)
+    created_at = models.DateTimeField(
+        verbose_name='дата создания заметки',
+        auto_now_add=True)
+    updated_at = models.DateTimefield(
+        verbose_name='дата последнего обновления заметки',
+        auto_now=True)
+    author = models.OneToOneField(
+        TodoUser,
+        on_delete=models.CASCADE,
+        verbose_name='автор заметки')
+
+    def __str__(self):
+        return f'Заметка: {self.title}'
 
