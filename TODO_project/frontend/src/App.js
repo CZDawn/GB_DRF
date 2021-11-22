cee895bc50caa3c37fe3d70efd9e88322ff2ab1b
codezerodawn <codezerodawn@gmail.com>
2021-11-22 20:14:28 +0300

    Done home task

--- TODO_project/frontend/src/App.js
+++ TODO_project/frontend/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios'
-import HeaderNav from './components/header.js'
-import FooterNav from './components/footer.js'
-import TodoUsersList from './components/TodoUsers.js'
-import ProjectsList from './components/Projects.js'
+import React from 'react';
+import logo from './logo.svg';
+import LogiForm from './components/Auth.js'
 import TODOsList from './components/TODOs.js'
+import FooterNav from './components/footer.js'
+import HeaderNav from './components/header.js'
+import ProjectsList from './components/Projects.js'
+import TodoUsersList from './components/TodoUsers.js'
 import {BrowserRouter, Route, Link, Switch} from 'react-router-dom'
 
 
@@ -15,62 +16,87 @@ class App extends React.Component {
         super(props)
         this.state = {
             'todo_users': [],
-            'projects': []
+            'projects': [],
+            'token': ''
         }
     }
 
-    componentDidMount() {
+    set_token(token) {
+        const cookies = new Cookies()
+        cookies.set('token', token)
+        this.setState({'token': token})
+    }
+
+    is_authenticated() {
+        return this.state.token != ''
+    }
+
+    logout() {
+        this.set_token('')
+    }
+
+    get_token_from_storage() {
+        const cookies = new Cookies()
+        const token = cookies.get('token')
+        this.setState({'token': token})
+    }
+
+    get_token(username, password) {
+        axios.post('http://127.0.0.1:8000/api-token-auth/', {username: username, password: password}).then(response => {
+            this.set_token(response.data['token'])
+        }).catch(error => alert('Неверный логин или пароль'))
+    }
+
+    load_data() {
         axios.get('http://127.0.0.1:8000/api/users/').then(response => {
-            const todo_users = response.data
-            this.setState(
-               {'todo_users': todo_users.results}
-            )
+            this.setState({'todo_users': response.data})
         }).catch(error => console.log(error))
 
         axios.get('http://127.0.0.1:8000/api/project/').then(response => {
-            const projects = response.data
-            this.setState(
-                {'projects': projects.results}
-            )
+            this.setState({'projects': projects.results})
         }).catch(error => console.log(error))
 
         axios.get('http://127.0.0.1:8000/api/todo/').then(response => {
-            const todo = response.data
-            this.setState(
-                {'todo': todo.results}
-            )
+            this.setState({'todo': todo.results})
         }).catch(error => console.log(error))
+    }
+
+    componentDidMount() {
+        this.get_token_from_storage()
+        this.load_data()
     }
 
     render () {
         return (
-            <div class="wrapper">
+            <div className="wrapper">
                 <BrowserRouter>
-                    <header class="header_container">
-                        <nav class="header_nav">
-                            <Link to='/' class="header_nav_item">
+                    <header className="header_container">
+                        <nav className="header_nav">
+                            <Link to='/' className="header_nav_item">
                                 Пользователи
                             </Link>
-                            <Link to='/project' class="header_nav_item">
+                            <Link to='/project' className="header_nav_item">
                                 Проекты
                             </Link>
-                            <Link to='/todo' class="header_nav_item">
+                            <Link to='/todo' className="header_nav_item">
                                 Заметки
                             </Link>
+                            {this.is_authenticated() ? <button onClick={()=>this.logout()}>Logout</button> : <Link to='/login'>Login</Link>}
                         </nav>
                     </header>
                     <Route exact path='/' component={() => <TodoUsersList TodoUsers={this.state.todo_users}/>}/>
                     <Route exact path='/project' component={() => <ProjectsList Projects={this.state.projects}/>}/>
                     <Route exact path='/todo' component={() => <TODOsList TODOs={this.state.todo}/>}/>
-                    <footer class="footer_container">
-                        <nav class="footer_nav">
-                            <Link to='/contacts' class="footer_nav_item">
+                    <Route exact path='/login' component={() => <LoginForm get_token={(username, password) => this.get_token(username, password)}/>}/>
+                    <footer className="footer_container">
+                        <nav className="footer_nav">
+                            <Link to='/contacts' className="footer_nav_item">
                                 Мои контакты
                             </Link>
-                            <Link to='/socials' class="footer_nav_item">
+                            <Link to='/socials' className="footer_nav_item">
                                 Мои социальные сети
                             </Link>
-                            <p class="footer_copyright">
+                            <p className="footer_copyright">
                                 Все права защищены &#169; 2021
                             </p>
                         </nav>
