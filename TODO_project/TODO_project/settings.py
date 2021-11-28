c05adef09e1c0f9c68435ba43d4bfed5f9a74d5d
codezerodawn <codezerodawn@gmail.com>
2021-11-28 22:16:46 +0300

    Done home task for lesson_9

--- TODO_project/TODO_project/settings.py
+++ TODO_project/TODO_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'django_filters',
     'rest_framework.authtoken',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
@@ -151,7 +152,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': \
         'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ],
@@ -160,6 +163,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_VERSIONING_CLASS': \
+        'rest_framework.versioning.QueryParameterVersioning',
 }
 
 if DEBUG:
